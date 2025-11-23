50ecfe32b7b334a89ec5892eb4cf22efb32b1cf1
Alex Klimerman <klimermana@gmail.com>
2025-11-22 22:17:56 -0800

    Add replacing

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,24 @@
-use anyhow::anyhow;
+use anyhow::{Context, anyhow};
 use clap::Parser;
+use flate2::Compression;
 use flate2::read::GzDecoder;
+use flate2::write::GzEncoder;
 use humansize::{BINARY, format_size};
+use itertools::Itertools;
 use serde::{Deserialize, Serialize};
 use sha2::{Digest, Sha256};
+use walkdir::WalkDir;
 use std::cmp::Reverse;
 use std::collections::HashMap;
 use std::fs::File;
-use std::path::PathBuf;
+use std::io::copy;
+use std::path::{Path, PathBuf};
 use std::{fs, io::BufReader, io::Read};
-use tar::Archive;
+use tar::{Archive, Builder};
 use tempfile::TempDir;
 use tempfile::tempdir;
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Clone, Deserialize, Serialize)]
 #[serde(rename_all = "PascalCase")]
 pub struct Manifest {
     pub config: String,
@@ -32,6 +37,27 @@ pub struct FileInfo {
 }
 
 #[derive(Debug, Clone)]
+pub struct DuplicateInfo {
+    pub original: FileInfo,
+    pub duplicates: Vec<FileInfo>,
+    pub total_savings: u64,
+}
+
+#[derive(Debug)]
+pub enum LinkType {
+    Sym,
+    Hard,
+}
+
+#[derive(Debug)]
+pub struct DeDupTransaction {
+    layer: usize,
+    original_path: String,
+    target_path: String,
+    link_type: LinkType,
+}
+
+#[derive(Debug, Clone)]
 pub struct Layer {
     pub path: PathBuf,
     pub layer_index: usize,
@@ -41,6 +67,14 @@ pub struct Analyzer {
     pub tmp_dir: TempDir,
     pub layers: Vec<Layer>,
     pub min_size: u64,
+    original_manifest: Manifest,
+}
+
+fn is_gzipped(file_path: &Path) -> Result<bool, Box<dyn std::error::Error>> {
+    let mut file = File::open(file_path)?;
+    let mut magic_bytes = [0u8; 2];
+    file.read_exact(&mut magic_bytes)?;
+    Ok(magic_bytes[0] == 0x1f && magic_bytes[1] == 0x8b)
 }
 
 impl Analyzer {
@@ -88,6 +122,7 @@ impl Analyzer {
             tmp_dir,
             layers,
             min_size,
+            original_manifest: manifest,
         })
     }
 
@@ -104,22 +139,19 @@ impl Analyzer {
     }
 
     fn scan_layer(&self, layer: &Layer) -> Result<Vec<FileInfo>, Box<dyn std::error::Error>> {
-        let file = File::open(layer.path.as_path())?;
-        if let Ok(files) = self.try_scan_gzipped(file, layer.layer_index) {
-            return Ok(files);
+        println!(
+            "Scanning layer {}/{}...",
+            layer.layer_index + 1,
+            self.layers.len()
+        );
+        if is_gzipped(&layer.path)? {
+            let file = File::open(layer.path.as_path())?;
+            let decoder = GzDecoder::new(file);
+            self.scan_tar_archive(decoder, layer.layer_index)
+        } else {
+            let file = File::open(layer.path.as_path())?;
+            self.scan_tar_archive(file, layer.layer_index)
         }
-
-        let file = File::open(layer.path.as_path())?;
-        self.scan_tar_archive(file, layer.layer_index)
-    }
-
-    fn try_scan_gzipped<R: Read>(
-        &self,
-        reader: R,
-        layer_index: usize,
-    ) -> Result<Vec<FileInfo>, Box<dyn std::error::Error>> {
-        let decoder = GzDecoder::new(reader);
-        self.scan_tar_archive(decoder, layer_index)
     }
 
     fn scan_tar_archive<R: Read>(
@@ -150,9 +182,7 @@ impl Analyzer {
             }
 
             let mut hasher = Sha256::new();
-            let mut buffer = Vec::new();
-            entry.read_to_end(&mut buffer)?;
-            hasher.update(&buffer);
+            copy(&mut entry, &mut hasher)?;
             let hash = format!("{:x}", hasher.finalize());
 
             files.push(FileInfo {
@@ -165,39 +195,235 @@ impl Analyzer {
         Ok(files)
     }
 
-    pub fn find_duplicates(&self) -> Result<Vec<FileInfo>, Box<dyn std::error::Error>> {
+    pub fn find_duplicates(&self) -> Result<Vec<DuplicateInfo>, Box<dyn std::error::Error>> {
         let files = self.scan_files()?;
-        //println!("{:#?}", files);
-
-        //HashMap for Sha -> FileInfo
-        let mut seen_files = HashMap::new();
-        // Vec of duplicates
-        let mut duplicates = Vec::new();
-        for file_info in files {
-            if seen_files.contains_key(&file_info.hash) {
-                duplicates.push(file_info);
-            } else {
-                seen_files.insert(file_info.hash.clone(), file_info);
-            }
+        let mut files_by_hash: HashMap<String, Vec<FileInfo>> = HashMap::new();
+        for file in files {
+            files_by_hash
+                .entry(file.hash.clone())
+                .or_default()
+                .push(file);
         }
-        Ok(duplicates)
+        Ok(files_by_hash
+            .into_iter()
+            .filter(|(_, files)| files.len() > 1)
+            .map(|(_, mut files)| {
+                files.sort_by_key(|f| f.layer_index);
+                let target = files.remove(0);
+                let savings = target.size * files.len() as u64;
+                DuplicateInfo {
+                    original: target,
+                    duplicates: files,
+                    total_savings: savings,
+                }
+            })
+            .sorted_by_key(|d| Reverse(d.total_savings))
+            .collect())
     }
 
-    fn print_possible_savings(&self) -> Result<(), Box<dyn std::error::Error>> {
-        let mut duplicates = self.find_duplicates()?;
-        duplicates.sort_by_key(|d| Reverse(d.size));
+    fn print_possible_savings(
+        &self,
+        duplicates: &Vec<DuplicateInfo>,
+    ) -> Result<(), Box<dyn std::error::Error>> {
         println!("=============================");
         println!("Total duplicate files: {}", duplicates.len());
         println!(
             "Total duplicate size: {}",
-            format_size(duplicates.iter().map(|f| f.size).sum::<u64>(), BINARY)
+            format_size(
+                duplicates.iter().map(|f| f.total_savings).sum::<u64>(),
+                BINARY
+            )
         );
         println!("=============================");
         println!("Duplicate files:");
-        for dup in duplicates {
-            println!("\t{}, size: {}", dup.path, format_size(dup.size, BINARY))
+        for dup_info in duplicates.iter() {
+            println!(
+                "\tOriginal: {}, layer: {} size: {}",
+                dup_info.original.path,
+                dup_info.original.layer_index,
+                format_size(dup_info.original.size, BINARY)
+            );
+            for dup in dup_info.duplicates.iter() {
+                println!("\tDuplicate: {}, layer: {}", dup.path, dup.layer_index);
+            }
         }
         println!("=============================");
+        Ok(())
+    }
+
+    pub fn generate_modification_plan(
+        &self,
+        duplicates: Vec<DuplicateInfo>,
+    ) -> Result<HashMap<usize, Vec<DeDupTransaction>>, Box<dyn std::error::Error>> {
+        Ok(duplicates
+            .iter()
+            .flat_map(|d| d.duplicates.iter().map(move |f| (d, f)))
+            .map(|(d, f)| {
+                (
+                    f.layer_index,
+                    DeDupTransaction {
+                        layer: f.layer_index,
+                        original_path: d.original.path.clone(),
+                        target_path: f.path.clone(),
+                        link_type: if d.original.layer_index == f.layer_index {
+                            LinkType::Hard
+                        } else {
+                            LinkType::Sym
+                        },
+                    },
+                )
+            })
+            .into_group_map())
+    }
+
+    fn extract_layer(
+        &self,
+        layer_tar: &Path,
+        dest: &Path,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        let file = File::open(layer_tar)?;
+        if is_gzipped(&layer_tar)? {
+            let decoder = GzDecoder::new(file);
+            let mut archive = Archive::new(decoder);
+            archive.unpack(dest)?;
+        } else {
+            let mut archive = Archive::new(file);
+            archive.unpack(dest)?;
+        }
+        Ok(())
+    }
+
+    fn process_layer(
+        &self,
+        layer: &Layer,
+        modifications: &Vec<DeDupTransaction>,
+        output_dir: &Path,
+    ) -> Result<Layer, Box<dyn std::error::Error>> {
+        println!(
+            "Layer {}, modifications {:#?}",
+            layer.layer_index, modifications
+        );
+        let extract_dir = tempdir()?;
+        self.extract_layer(&layer.path, extract_dir.path())?;
+        for modif in modifications {
+            let file_path = extract_dir.path().join(&modif.target_path);
+            let _ = fs::remove_file(&file_path);
+            let source_path = PathBuf::from(&modif.original_path);
+            match modif.link_type {
+                LinkType::Sym => {
+                    std::os::unix::fs::symlink(&source_path, &file_path).context(format!(
+                        "Failed to create symlink {} -> {}",
+                        file_path.display(),
+                        source_path.display()
+                    ))?;
+                }
+                LinkType::Hard => {
+                    fs::hard_link(&source_path, &file_path).context(format!(
+                        "Failed to create symlink {} -> {}",
+                        file_path.display(),
+                        source_path.display()
+                    ))?;
+                }
+            }
+        }
+        let new_layer_filename = format!("layer-{}.tar.gz", layer.layer_index);
+        let new_layer_path = output_dir.join(&new_layer_filename);
+        let tar_file = File::create(&new_layer_path)?;
+
+        let encoder = GzEncoder::new(tar_file, Compression::default());
+        let mut builder = Builder::new(encoder);
+
+        builder.follow_symlinks(false);
+        builder
+            .append_dir_all("", extract_dir.path())
+            .context("Failed to pack layer")?;
+
+        builder.finish().context("Failed to finalize tar")?;
+
+        Ok(Layer {
+            path: new_layer_path,
+            layer_index: layer.layer_index,
+        })
+    }
+
+    fn update_manifest(
+        &self,
+        new_image_dir: &Path,
+        new_layers: &Vec<Layer>,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        let blobs_dir = new_image_dir.join("blobs/sha256");
+        fs::create_dir_all(&blobs_dir)?;
+
+        let mut new_refs = Vec::new();
+        for (idx, layer) in new_layers.iter().enumerate() {
+            let mut file = File::open(&layer.path)?;
+            let mut hasher = Sha256::new();
+            std::io::copy(&mut file, &mut hasher)?;
+            let digest = format!("{:X}", hasher.finalize());
+            let blob_path = blobs_dir.join(&digest);
+            fs::copy(&layer.path, &blob_path)?;
+
+            let relative_path = format!("blobs/sha256/{}", digest);
+            new_refs.push(relative_path);
+        }
+        let mut new_manifest = self.original_manifest.clone();
+        new_manifest.layers = new_refs;
+        let manifests = vec![new_manifest];
+        let new_manifest_path = new_image_dir.join("manifest.json");
+        let manifest_json = serde_json::to_string_pretty(&manifests)?;
+        fs::write(new_manifest_path, manifest_json)?;
+        Ok(())
+    }
+
+    pub fn create_deduplicated_image(
+        &self,
+        duplicates: Vec<DuplicateInfo>,
+        output_path: &Path,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        let work_dir = tempdir()?;
+        let work_path = work_dir.path();
+        let new_layer_dir = work_path.join("new_layers");
+        let staging_dir = work_path.join("staging");
+        fs::create_dir(&new_layer_dir)?;
+        let mut plan = self.generate_modification_plan(duplicates)?;
+        println!("{:#?}", plan);
+
+        let mut new_layers = Vec::new();
+        for layer in &self.layers {
+            println!(
+                "Processing layer {}/{}",
+                layer.layer_index + 1,
+                self.layers.len()
+            );
+            let layer_mods = plan.entry(layer.layer_index).or_default();
+            if layer_mods.is_empty() {
+                new_layers.push(layer.clone());
+            } else {
+                new_layers.push(self.process_layer(layer, layer_mods, &new_layer_dir)?);
+            }
+        }
+
+        self.update_manifest(&staging_dir, &new_layers)?;
+
+        let config_src = self.tmp_dir.path().join(&self.original_manifest.config);
+        let config_dst = staging_dir.join(&self.original_manifest.config);
+
+        fs::copy(config_src, config_dst)?;
+
+        let output_file = File::create(output_path).context(format!(
+            "Failed to create output file: {}",
+            output_path.display()
+        ))?;
+
+        let mut builder = Builder::new(output_file);
+
+        // Add all files from the new image directory
+        builder
+            .append_dir_all(".", staging_dir)
+            .context("Failed to pack final image")?;
+
+        builder.finish().context("Failed to finalize output tar")?;
+
         Ok(())
     }
 }
@@ -209,6 +435,9 @@ struct Args {
     #[arg(short, long)]
     image: String,
 
+    #[arg(short, long)]
+    output: String,
+
     /// minimum size of an object to track
     #[arg(short, long, default_value_t = 1_000_000)]
     min_size: u64,
@@ -219,7 +448,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Running on image: {}", args.image);
 
     let analyzer = Analyzer::load(args.image, args.min_size)?;
-
-    let _ = analyzer.print_possible_savings();
+    let duplicates = analyzer.find_duplicates()?;
+    let _ = analyzer.print_possible_savings(&duplicates);
+    let _ = analyzer.create_deduplicated_image(duplicates, Path::new(&args.output))?;
     Ok(())
 }
